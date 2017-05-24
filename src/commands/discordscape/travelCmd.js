b1d79f1c79683eb97d33bffd34008b3d9341a513
Binguu <csh95@comcast.net>
2017-05-24 18:08:03 -0400

    Edited explore values of UMBRIDGE and AL_KHAREED in locations.js
    Edited travelCmd so that users can now enter location names with or without underscores or spaces.

--- src/commands/discordscape/travelCmd.js
+++ src/commands/discordscape/travelCmd.js
@@ -11,19 +11,28 @@ module.exports = class TravelCommand extends graf.Command {
             description: 'Move that booty somewhere else.',
             usage: 'travel',
             details: 'Moves the player to specified location.',
-            examples: ['travel ZARROCK']
+            examples: ['travel ZARROCK'],
+            argType: 'single'
         });
     }
 
     run(message, args) {
         const player_id = message.author.id;
-
+        var givenLoc = args[0];
+        while (givenLoc.includes('_') || givenLoc.includes(' ')) {
+            givenLoc = givenLoc.replace('_', '').replace(' ', '');
+        }
         return storage.getItem(player_id)
             .then( (player) => {
-                player.location.name = args[0];
-                storage.setItem(player.location.name, args[0])
+                var newLoc;
+                for (var i = 0; i < LOCS.enumValues.length; i++) {
+                    if (LOCS.enumValues[i].name.replace('_', '') === givenLoc.toUpperCase()) {
+                        newLoc = LOCS.enumValues[i];
+                    }
+                }
+                player.location = newLoc;
+                return storage.setItem(player_id, player)
                     .then(() => {
-                        //console.log(player.location);
                         return `Welcome to the town of ${player.location.name}!`;
                     })
                     .catch ( (err) => {
